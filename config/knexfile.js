e78c2500cfa0e5f6bb04b6af18d2f9902fb2482d
Ous Bayaa <bayaaous@gmail.com>
2020-08-13 12:31:05 -0700

    formatted knex.js client name

--- config/knexfile.js
+++ config/knexfile.js
@@ -2,34 +2,35 @@ var dotenv = require('dotenv');
 dotenv.config({ path: '../.env' });
 
 module.exports = {
-  //   development: {
-  //     client: 'pg',
-  //     connection: process.env.DATABASE_URL,
-  //     migrations: { directory: '../data/migrations' },
-  //     seeds: { directory: '../data/seeds' },
-  //     pool: {
-  //       min: 2,
-  //       max: 10,
-  //     },
-  //   },
-
-  //   test: {
-  //     client: 'pg',
-  //     connection: process.env.DATABASE_URL,
-  //     migrations: { directory: '../data/migrations' },
-  //     seeds: { directory: '../data/seeds' },
-  //   },
-
-  //   production: {
-  //     client: 'pg',
-  //     connection: process.env.DATABASE_URL,
-  //     migrations: { directory: '../data/migrations' },
-  //     seeds: { directory: '../data/seeds' },
-  //   },
   development: {
-    client: 'postgresql',
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: '../data/migrations' },
+    seeds: { directory: '../data/seeds' },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: '../data/migrations' },
+    seeds: { directory: '../data/seeds' },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: '../data/migrations' },
+    seeds: { directory: '../data/seeds' },
+  },
+
+  local: {
+    client: 'pg',
     connection: {
-      user: 'postgres',
+      user: 'postgresql',
       password: '',
     },
     migrations: {
