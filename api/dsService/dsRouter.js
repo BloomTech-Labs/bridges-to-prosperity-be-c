bb2c4192e9a901d7e73041d3d84a1262d991a89d
Ous Bayaa <bayaaous@gmail.com>
2020-09-02 09:42:42 -0700

    updated dsrouter to correct npm run lint error in github

--- api/dsService/dsRouter.js
+++ api/dsService/dsRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const dsModel = require('./dsModel');
 const authRequired = require('../middleware/authRequired');
-const db = require('../../data/db-config');
 
 /**
  * @swagger
