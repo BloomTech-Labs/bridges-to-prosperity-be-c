36ec645247840430e8c3055b595b355d195b18fb
ferror18 <ferror18@gmail.com>
2020-10-06 14:07:01 -0700

    Merged final.csv and bridgePic.csv

--- postImages/index.py
+++ postImages/index.py
@@ -1,12 +1,26 @@
 import csv
 import requests
 bi = open("bridgeData3.csv",mode='r').readlines()
-bd = open("final.csv",mode='r').readlines()
 finalcsv = bi[1:]
 bid = {}
 for i in bi[1:]:
     x = i.split(',')
-    bid[x[1]] = x[2:]
+    bid[x[1]] = ','.join(x[2:])
+# bi.close()
+# print(bid)
 
+bd = open("final.csv",mode='r').readlines()
+bw = open('data_and_img.csv', mode='w')
+# print(bd)
+bd = bd[1:]
 for i in bd:
-    pass
+    x = i
+    i = i.split(',')
+    i = i[6]
+    if i in bid:
+        print(i)
+        x = x + bid[i]
+    print(x)
+    bw.write(x)
+
+bw.close()
