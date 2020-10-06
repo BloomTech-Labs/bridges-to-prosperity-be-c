94274388562cc03e600911ac2b48714a6af9869d
ferror18 <ferror18@gmail.com>
2020-10-06 10:41:26 -0700

    Cleared print statements and commented code

--- postImages/index.py
+++ postImages/index.py
@@ -1,6 +1,12 @@
-import pandas as pd
- 
+import csv
+import requests
+bi = open("bridgeData3.csv",mode='r').readlines()
+bd = open("final.csv",mode='r').readlines()
+finalcsv = bi[1:]
+bid = {}
+for i in bi[1:]:
+    x = i.split(',')
+    bid[x[1]] = x[2:]
 
-df = pd.read_csv("bridgeData3.csv")
-
-print(df.iloc[2,0:5])
+for i in bd:
+    pass
