c3cfe5a966d1cb879288b620fceb6c121d9c9e71
ferror18 <ferror18@gmail.com>
2020-10-07 11:56:30 -0700

    new json data

--- postImages/index.py
+++ postImages/index.py
@@ -13,14 +13,23 @@ bd = open("final.csv",mode='r').readlines()
 bw = open('data_and_img.csv', mode='w')
 # print(bd)
 bd = bd[1:]
+bw.write('province,district,sector,cell,bridge_site_name,project_stage,project_code,bridge_type,latitud,longitud,individuals_served,assessment_date,original_community,community_served,prov_id,district_id,sector_id,cell_id,pre_photo,after_photo')
 for i in bd:
     x = i
+    x = x.split(',')
+    x.pop(11)
+    x = ','.join(x)
+    # print(x)
+
     i = i.split(',')
     i = i[6]
     if i in bid:
-        print(i)
-        x = x + bid[i]
-    print(x)
+        # print(i)
+        x = x[:-1]+','+ bid[i]
+    # print(x)
+    else:
+        x = x[:-1]+','+ 'null,null'
+        # print(x)
     bw.write(x)
 
 bw.close()
