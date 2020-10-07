0369138ac10656649685320de78b4b3ff653b3f3
ferror18 <ferror18@gmail.com>
2020-10-07 13:16:03 -0700

    Re made schema

--- data/migrations/20200812122516_dummy_data.js
+++ data/migrations/20200812122516_dummy_data.js
@@ -1,6 +1,9 @@
 exports.up = (knex) => {
   return knex.schema.createTable('bridges', function (table) {
+    table.increments();
     table.integer('project_code').notNullable().unique();
+    table.string('after_img');
+    table.string('before_img');
     table.string('province');
     table.string('district');
     table.string('sector');
@@ -8,12 +11,17 @@ exports.up = (knex) => {
     table.string('bridge_site_name');
     table.string('project_stage');
     table.string('bridge_type');
-    table.float('latitude').notNullable();
-    table.float('longitude').notNullable();
-    table.integer('individuals_served');
-    table.string('project_assesment');
-    table.string('bridge_image_before');
-    table.string('bridge_image_after');
+    table.string('latitude');
+    table.string('longitude');
+    table.string('individuals_directly_served');
+    table.string('assessment_date');
+    table.string('original_community_col');
+    table.string('community_served');
+    table.integer('prov_id');
+    table.integer('district_id');
+    table.integer('sector_id');
+    table.integer('cell_id');
+
   });
 };
 
